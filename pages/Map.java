5e08344ab8ef2d60275469253e7aaefc8fcec546
sniveeey <thaddeusf1208@gmail.com>
2025-05-20 22:08:51 -0500

    alex and map

--- pages/Map.java
+++ pages/Map.java
@@ -1,10 +1,19 @@
 package pages;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class Map {
     public static Scene getScene() {
-        return new Scene(new VBox(), 640, 480);
+        VBox root = new VBox();
+        Image image = new Image("map.png", 1280, 960, false, false);
+        // idk why but doubling it got rid of the smoothing
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(640);
+        imageView.setFitHeight(480);
+        root.getChildren().add(imageView);
+        return new Scene(root, 640, 480);
     }
 }
